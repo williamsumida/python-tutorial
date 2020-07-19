72241aa7c4b1e661d2d8b4f0afd022bd26af8f6d
William <william.h.sumida@gmail.com>
2020-07-19 07:05:51 -0300

    first commit

---
+++ 1 - conceitos/operacoes_com_variaveis.py
@@ -0,0 +1,45 @@
+# Operacoes numericas
+# 1 - valores inteiros
+print('valores inteiros')
+x = 100
+y = 30
+
+print('Soma: x + y')
+print(x + y)
+
+print('Subtracao: x - y')
+print(x - y)
+
+print('Multiplicacao: x * y')
+print(x * y)
+
+print('Divisao: x / y')
+print(x / y)
+
+print('Resto da divisao: x/y')
+print(x % y)
+
+
+# 2 - valores reais
+print('\nvalores reais')
+x = 100.0
+y = 30.0
+
+print('Soma: x + y')
+print(x + y)
+
+print('Subtracao: x - y')
+print(x - y)
+
+print('Multiplicacao: x * y')
+print(x * y)
+
+print('Divisao: x / y')
+print(x / y)
+
+print('Resto da divisao: x/y')
+print(x % y)
+
+
+# 3 - valores alfanumericos
+
---
+++ 1 - conceitos/tipos_de_dados.py
@@ -0,0 +1,29 @@
+
+# 1 - Dados numericos
+#   1.1 - Numeros Inteiros
+x = 10
+y = 15
+
+print(x)
+print(type(x))
+
+# 1.2 - Numeros Reais
+x = 10.0
+y = 15.0
+print(x)
+print(type(x))
+
+# 2 - Dados alfanumericos
+nome = "Ze"
+tipo = "Pessoa"
+print(nome)
+print(type(nome))
+
+# 3 - Dados booleanos
+verdadeiro = True
+falso = False
+
+# 4 - Conjuntos de dados
+lista = []
+tupla = ()
+dicionario = {}
